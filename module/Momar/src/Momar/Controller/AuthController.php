5afa3612f2b96d9853ac296804822c01203476c6
Romont Johnson <rome2213@gmail.com>
2013-03-26 07:15:50 -0400

    module

--- module/Momar/src/Momar/Controller/AuthController.php
+++ module/Momar/src/Momar/Controller/AuthController.php
@@ -17,52 +17,48 @@ class AuthController extends AbstractRestfulController {
     public function get($id) {
         return new JsonModel(array("data" => 0));
     }
-    
+
     public function getList() {
         return new JsonModel(array("data" => 0));
     }
-    
+
     public function create($data) {
         $form = new LoginForm();
 
-        /*$request = $this->getRequest();
-        if ($request->isPost()) {
-            $form->setData($request->getPost());
-            if ($form->isValid()) {
-                //check authentication...
-                $this->getAuthService()->getAdapter()
-                        ->setIdentity($request->getPost($data->email))
-                        ->setCredential($request->getPost($data->password));
+        $form->setData($data);
+        if ($form->isValid()) {
+            //check authentication...
+            $this->getAuthService()->getAdapter()
+                    ->setIdentity($data->email)
+                    ->setCredential($data->password);
 
-                $result = $this->getAuthService()->authenticate();
-                foreach ($result->getMessages() as $message) {
-                    //save message temporary into flashmessenger
-                    $this->flashmessenger()->addMessage($message);
-                }
-
-                if ($result->isValid()) {
-                    //$redirect = 'application';
-                    //check if it has rememberMe :
-                    if ($request->getPost($data->rememberMe) == 1) {
-                        $this->getSessionStorage()
-                                ->setRememberMe(1);
-                        //set storage again
-                        $this->getAuthService()->setStorage($this->getSessionStorage());
-                    }
-                    $this->getAuthService()->getStorage()->write($request->getPost($data->email));
-                    return new JsonModel(array('success' => true));
-                } else {
-                    return new JsonModel(array('success' => false));
-                }
+            $result = $this->getAuthService()->authenticate();
+            foreach ($result->getMessages() as $message) {
+                //save message temporary into flashmessenger
+                $this->flashmessenger()->addMessage($message);
             }
-        }*/
-        return new JsonModel(array('success' => true));
+
+            if ($result->isValid()) {
+                //$redirect = 'application';
+                //check if it has rememberMe :
+                if ($data->rememberMe == 1) {
+                    $this->getSessionStorage()
+                            ->setRememberMe(1);
+                    //set storage again
+                    $this->getAuthService()->setStorage($this->getSessionStorage());
+                }
+                $this->getAuthService()->getStorage()->write($data->email);
+                return new JsonModel(array('success' => true));
+            } else {
+                return new JsonModel(array('success' => false));
+            }
+        }
     }
-    
+
     public function update($id, $data) {
         return new JsonModel(array("data" => 0));
     }
-    
+
     public function delete($id) {
         return new JsonModel(array("data" => 0));
     }
@@ -70,7 +66,7 @@ class AuthController extends AbstractRestfulController {
     public function getAuthService() {
         if (!$this->authservice) {
             $this->authservice = $this->getServiceLocator()
-                            ->get('AuthService');
+                    ->get('AuthService');
         }
 
         return $this->authservice;
@@ -79,7 +75,7 @@ class AuthController extends AbstractRestfulController {
     public function getSessionStorage() {
         if (!$this->storage) {
             $this->storage = $this->getServiceLocator()
-                            ->get('Login\Model\MyAuthStorage');
+                    ->get('Login\Model\MyAuthStorage');
         }
 
         return $this->storage;
